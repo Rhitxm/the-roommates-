ab62d39d18709a3a50eb7d9fd4d3faf329806f2f
Sumedh2509 <sumedh.ghule123@gmail.com>
2026-09-12 19:29:49 +0530

    Set up Flask project structure and Jinja templates

--- script.py
+++ script.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask , render_template #importing things
 
-app = Flask(__name__)
+app = Flask(__name__) #assigning the variable app
 
-@app.route("/")
+@app.route("/") #routing - /  means home page basically 
 def home():
-    return "Hello , this is portfolio of two roommates!"
+    roommates =[
+    {
+        "name": "Sumedh",
+        "instagram": "https://www.instagram.com/sumedhg.69/",
+        "linkedin": "https://www.linkedin.com/in/sumedh-ghule-790617342/",
+        "profile_image": "https://avatars.githubusercontent.com/u/190742018?v=4"
+    },
+    {
+        "name": "Rhitam",
+        "instagram": "https://www.instagram.com/rhitxm.exe/",
+        "linkedin": "https://www.linkedin.com/in/rhitam-paul-4a58021b4/",
+        "profile_image": "https://avatars.githubusercontent.com/u/293724039?v=4"
+    } ]#creating a list of people
+    return render_template("index.html", people= roommates ) #people= roommates , basically tells that use the strings in roommates list for the variable 'people'in our html file
+        #remember - the variable for html --> people , the variable that it is acessing from python-->roommates
 
-@app.route("/about")
+    
+
+@app.route("/about") #routing to another page
 def about_us():
     return "We are roommates at VESIT"
 
