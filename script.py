40c039b272b16e6bab506c05891cd75b9ed3faf9
Sumedh2509 <sumedh.ghule123@gmail.com>
2026-09-12 15:35:15 +0530

    Set up flask and routes

--- script.py
+++ script.py
@@ -1,1 +1,14 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello , this is portfolio of two roommates!"
+
+@app.route("/about")
+def about_us():
+    return "We are roommates at VESIT"
+
+if __name__ == "__main__":
+    app.run(debug=True)
