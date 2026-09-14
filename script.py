46920d33c749023937f523804e406299c93c5e89
Sumedh2509 <sumedh.ghule123@gmail.com>
2026-09-14 10:47:38 +0530

    generalized the html using jinja

--- script.py
+++ script.py
@@ -9,13 +9,15 @@ def home():
         "name": "Sumedh",
         "instagram": "https://www.instagram.com/sumedhg.69/",
         "linkedin": "https://www.linkedin.com/in/sumedh-ghule-790617342/",
-        "profile_image": "https://avatars.githubusercontent.com/u/190742018?v=4"
+        "profile_image": "https://avatars.githubusercontent.com/u/190742018?v=4",
+        "github": "https://github.com/Sumedh2509"
     },
     {
         "name": "Rhitam",
         "instagram": "https://www.instagram.com/rhitxm.exe/",
         "linkedin": "https://www.linkedin.com/in/rhitam-paul-4a58021b4/",
-        "profile_image": "https://avatars.githubusercontent.com/u/293724039?v=4"
+        "profile_image": "https://avatars.githubusercontent.com/u/293724039?v=4",
+        "github": "https://github.com/Rhitxm"
     } ]#creating a list of people
     return render_template("index.html", people= roommates ) #people= roommates , basically tells that use the strings in roommates list for the variable 'people'in our html file
         #remember - the variable for html --> people , the variable that it is acessing from python-->roommates
