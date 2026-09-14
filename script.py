09884148bbb48e915d37f8b15d25d1cd28b931db
Sumedh2509 <sumedh.ghule123@gmail.com>
2026-09-14 11:38:38 +0530

    file name change

--- script.py
+++ script.py
@@ -19,7 +19,7 @@ def home():
         "profile_image": "https://avatars.githubusercontent.com/u/293724039?v=4",
         "github": "https://github.com/Rhitxm"
     } ]#creating a list of people
-    return render_template("index.html", people= roommates ) #people= roommates , basically tells that use the strings in roommates list for the variable 'people'in our html file
+    return render_template("index1.html", people= roommates ) #people= roommates , basically tells that use the strings in roommates list for the variable 'people'in our html file
         #remember - the variable for html --> people , the variable that it is acessing from python-->roommates
 
     
